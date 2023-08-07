bafdcbdb06df560f80d9c84f49458fa9408f137f
lavezzo <guilherm909088@gmail.com>
2023-08-06 23:18:25 -0300

    Update requirementes

--- database.py
+++ database.py
@@ -2,13 +2,9 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-# SQLALCHEMY_DATABASE_URL = "sqlite:///db.sqlite3"
 SQLALCHEMY_DATABASE_URL = "mssql+pyodbc://mamutesdb:MamutesDatabase2023@mamutes-database.database.windows.net:1433/DbMamutesParty?driver=ODBC+Driver+17+for+SQL+Server"
 
-
-engine = create_engine(
-    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
-)
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 
